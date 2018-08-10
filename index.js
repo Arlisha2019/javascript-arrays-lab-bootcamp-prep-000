4aed6b8f93fbee8e0488f57f99a7643a47315474
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-10 22:20:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ return [...kittens, name];
 }
 
 function prependKitten(name) {
- return kittens.slice(0, kittens.length - 1);
+ return kittens.slice(0, kittens.length + 1);
 }
 
 // Add your functions and code here
