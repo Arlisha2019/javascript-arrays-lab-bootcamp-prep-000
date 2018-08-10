b8680d6da39c7906b8b8c26754c6b33b3451d3a3
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-10 21:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-var kittens = new newKittens(name);
+var kittens = new kittens(name);
 }
 
 // Add your functions and code here
